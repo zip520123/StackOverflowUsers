c1b2ae084c8c7bdbedb54f853d8f6fe3b54d9a06
zip520123 <zip520123@gmail.com>
2025-08-04 20:24:21 +0100

    Uses constructor injection for URLSession

--- StackOverflowUsers/Service/NetworkService.swift
+++ StackOverflowUsers/Service/NetworkService.swift
@@ -6,13 +6,18 @@ protocol NetworkServiceProtocol {
 
 class NetworkService: NetworkServiceProtocol {
     private let urlString = "https://api.stackexchange.com/2.2/users?page=1&pagesize=20&order=desc&sort=reputation&site=stackoverflow"
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
     
     func fetchTopUsers(completion: @escaping (Result<[User], Error>) -> Void) {
         guard let url = URL(string: urlString) else {
             completion(.failure(NSError(domain: "Invalid URL", code: 0)))
             return
         }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
                 return
